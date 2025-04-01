f969985867b95340c1a3981ae67d4e872b9a0f97
romaniuk <romaniuk.maksym.d@chnu.edu.ua>
2025-04-01 08:54:25 +0300

    first task

---
+++ task1/task1/task1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+void buildArrayC(const std::vector<int>& A) {
+    std::vector<int> C(A.size());
+
+    for (size_t i = 0; i < A.size(); i++) {
+        C[i] = pow(2, A[i]); // Формула c_i = 2^a_i
+    }
+
+    std::cout << "Array C: ";
+    for (int val : C) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::vector<int> A = { 1, 3, 5, 2, 7, 4 };
+    buildArrayC(A);
+    return 0;
+}
